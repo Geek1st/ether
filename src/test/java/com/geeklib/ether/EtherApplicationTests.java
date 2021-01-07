4dd51aadeef865b20ebcf3d5b7489407c2460bd2
geek1st <geek1st@live.com>
2021-01-07 23:19:19 +0800

    init

---
+++ src/test/java/com/geeklib/ether/EtherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geeklib.ether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
